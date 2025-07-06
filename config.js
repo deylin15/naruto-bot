0364e9de07be6426cb8ab3ca930cc9125b44b15e
Deylin <deylinbo91@gmail.com>
2025-07-06 17:59:54 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -7,78 +7,78 @@ import fetch from 'node-fetch'
 import axios from 'axios'
 import moment from 'moment-timezone' 
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-// ╭━━━╮╱╱╱╱╱╭╮╱╱╭━━━╮╱╱╱╭╮
-// ┃╭━╮┃╱╱╱╱╱┃┃╱╱┃╭━╮┃╱╱╱┃┃
-// ┃┃╱┃┣━━┳━━┫╰━╮┃┃╱┃┣━━┳┫┃╭┳━━┳━╮
-// ┃┃╱┃┃┃━┫┃━┫╭╮┃┃┃╱┃┃╭╮┣┫╰╯┫╭╮┃╭╯
-// ┃╰━╯┃┃━┫┃━┫┃┃┃┃╰━╯┃╭╮┃┃╭╮┫╭╮┃┃
-// ╰━━━┻━━┻━━┻╯╰╯╰━━━┻╯╰┻┻╯╰┻╯╰┻╯
-// ╭━╮╭━┳━━┳╮╱╭┳━━━┳━━━┳━╮╭━╮
-// ┃┃╰╯┃┣╮╭┫┃╱┃┃╭━╮┃╭━╮┃┃╰╯┃┃
-// ┃╭╮╭╮┃┃┃┃╰━╯┃╰━╯┃┃╱┃┃╭╮╭╮┃
-// ┃┃┃┃┃┃┃┃┃╭━╮┃╭━━┫╰━╯┃┃┃┃┃┃
-// ┃┃┃┃┃┣╯╰┫┃╱┃┃┃╱╱┃╭━╮┃┃┃┃┃┃
-// ╰╯╰╯╰┻━━┻╯╱╰┻╯╱╱╰╯╱╰┻╯╰╯╰╯
+//*⛩️━━━━━━━━━━━━━━━⛩️*
+//🌀 Naruto-Bot Config 🌀
+//⚔️ Código secreto de Konoha
+//*⛩️━━━━━━━━━━━━━━━⛩️*
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
-
-// ⚙️ PROPIETARIO Y STAFF
+// 👤 PROPIETARIOS Y ALDEANOS
 global.owner = [
-['50433191934', '🔰 Creador 🔰', true],
-['5217227584934', 'Angel', true],
-['573001533523', 'BʀᴀʏᴀɴXD', true],
-['5212202410659', 'Destroy', true],
+['50433191934', '🍥 Hokage Supremo', true]
 //['numero', 'nombre', true],
 ];
 
-//si no saben no toquen gracias 🫂 
+
 global.mods = ['50433191934'];
 global.suittag = ['50433191934'];
 global.prems = [];
 
-// 📚 INFORMACIÓN GENERAL
+
 global.libreria = 'Baileys';
 global.baileys = '@whiskeysockets/baileys';
-global.nameqr = 'Pikachu-Bot';
-global.namebot = 'Pikachu-Bit';
-global.sessions = 'Sessions';
-global.jadi = 'JadiBots';
+global.nameqr = 'Naruto-Bot';
+global.namebot = 'Naruto-Bit';
+global.sessions = 'AldeaSessions';
+global.jadi = 'ClonesJutsu';
 global.pikaJadibts = true;
 
-// ✨ DATOS DE ESTILO Y METADATOS
-global.packname = '🧃 Pikachu-Bot MD';
-global.botname = '⚡ Pikachu-Bot ⚡';
-global.wm = 'Pikachu-MD';
-global.dev = '© Desarrollado por Deylin';
-global.textbot = 'Pikachu-Bot • Potenciado por Deylin';
-global.etiqueta = 'Team Pikachu ⚡';
 
-// 💰 MONEDA Y AVATARES
-global.moneda = 'pikas';
+global.packname = '🌀 Naruto-Bot MD';
+global.botname = '🍥 Naruto-Bot 🍥';
+global.wm = 'Konoha-MD';
+global.dev = '🔥 Desarrollado por Deylin del Clan Uzumaki';
+global.textbot = 'Naruto-Bot • Chakra de Deylin';
+global.etiqueta = 'Clan Shinobi 🍥';
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
+
+global.moneda = 'ramens';
+
 
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
-global.photoSity = [catalogo]
+global.photoSity = [catalogo];
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.gp1 = 'https://chat.whatsapp.com/F8KwM3rVqkS9HhR5msoRqQ'
 global.channel2 = 'https://whatsapp.com/channel/0029VayQwPsFnSzESZJ9Us3z'
-global.md = 'https://github.com/Deylin-Eliac/Pikachu-bot'
+global.md = 'https://github.com/Deylin-Eliac/Naruto-Bot'
 global.correo = 'deylibaquedano801@gmail.com'
-global.cn ='https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m';
+global.cn = 'https://whatsapp.com/channel/0029VawF8fBBvvsktcInIz3m';
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.catalogo = fs.readFileSync('./src/catalogo.jpg');
-global.estilo = { key: {  fromMe: false, participant: `0@s.whatsapp.net`, ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) }, message: { orderMessage: { itemCount : -999999, status: 1, surface : 1, message: packname, orderTitle: 'Bang', thumbnail: catalogo, sellerJid: '0@s.whatsapp.net'}}}
+global.estilo = { 
+  key: {  
+    fromMe: false, 
+    participant: `0@s.whatsapp.net`, 
+    ...(false ? { remoteJid: "5219992095479-1625305606@g.us" } : {}) 
+  }, 
+  message: { 
+    orderMessage: { 
+      itemCount : -999999, 
+      status: 1, 
+      surface : 1, 
+      message: packname, 
+      orderTitle: 'Kage Bunshin no Jutsu', 
+      thumbnail: catalogo, 
+      sellerJid: '0@s.whatsapp.net'
+    }
+  }
+};
+
 global.ch = {
-ch1: '120363365444927738@newsletter',
+  ch1: '120363365444927738@newsletter',
 }
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 global.MyApiRestBaseUrl = 'https://api.cafirexos.com';
 global.MyApiRestApikey = 'BrunoSobrino';
@@ -90,14 +90,13 @@ global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c7
 global.keysxteam = keysxteammm[Math.floor(keysxteammm.length * Math.random())];
 global.keysneoxrrr = ['5VC9rvNx', 'cfALv5'];
 global.keysneoxr = keysneoxrrr[Math.floor(keysneoxrrr.length * Math.random())];
-global.lolkeysapi = ['kurumi']; // ['BrunoSobrino_2']
+global.lolkeysapi = ['kurumi'];
 global.itsrose = ['4b146102c4d500809da9d1ff'];
 
-//*─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─⭒─ׄ─ׅ─ׄ─*
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
   unwatchFile(file)
-  console.log(chalk.redBright("Update 'config.js'"))
+  console.log(chalk.redBright("🩸 ¡Archivo 'config.js' actualizado! (Modo sabio activado)"))
   import(`${file}?update=${Date.now()}`)
 })
