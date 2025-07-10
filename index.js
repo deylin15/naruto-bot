eab72882d2592a798cc83ef45c88e598277fa2fa
Deylin <deylinbo91@gmail.com>
2025-07-10 10:30:23 -0600

    Actualizar index.js

--- index.js
+++ index.js
@@ -156,32 +156,45 @@ version,
 global.conn = makeWASocket(connectionOptions);
 
 if (!fs.existsSync(`./${sessions}/creds.json`)) {
-if (opcion === '2' || methodCode) {
-opcion = '2'
-if (!conn.authState.creds.registered) {
-let addNumber
-if (!!phoneNumber) {
-addNumber = phoneNumber.replace(/[^0-9]/g, '')
-} else {
-do {
-phoneNumber = await question(chalk.bgBlack(chalk.bold.greenBright(`┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⍰
+  if (opcion === '2' || methodCode) {
+    opcion = '2'
+    if (!conn.authState.creds.registered) {
+      let addNumber
+
+      if (!!phoneNumber) {
+        addNumber = phoneNumber.replace(/[^0-9]/g, '')
+      } else {
+        do {
+          phoneNumber = await question(
+            chalk.bgBlack(
+              chalk.bold.greenBright(`┏╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍⍰
 ┃Por favor, Ingrese el número de WhatsApp.
 ┗┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄⍰\n${chalk.bold.yellowBright(`╭┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈╾
 │➙Ejemplo: 57321×××××××
-╰╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╾`)}\n${chalk.bold.magentaBright('---> ')}`)))
-phoneNumber = phoneNumber.replace(/\D/g,'')
-if (!phoneNumber.startsWith('+')) {
-phoneNumber = `+${phoneNumber}`
-}
-} while (!await isValidPhoneNumber(phoneNumber))
-rl.close()
-addNumber = phoneNumber.replace(/\D/g, '')
-setTimeout(async () => {
-let codeBot = await conn.requestPairingCode(addNumber)
-codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
-console.log(chalk.bold.white(chalk.bgMagenta(`✧ CÓDIGO DE VINCULACIÓN ✧`)), chalk.bold.white(chalk.white(codeBot)))
-}, 3000)
-}}}
+╰╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╍╾`)}\n${chalk.bold.magentaBright('---> ')}`)
+          )
+
+          phoneNumber = phoneNumber.replace(/\D/g, '')
+          if (!phoneNumber.startsWith('+')) {
+            phoneNumber = `+${phoneNumber}`
+          }
+
+        } while (!await isValidPhoneNumber(phoneNumber))
+
+        rl.close()
+        addNumber = phoneNumber.replace(/\D/g, '')
+
+        setTimeout(async () => {
+          let codeBot = await conn.requestPairingCode(addNumber)
+          codeBot = codeBot?.match(/.{1,4}/g)?.join("-") || codeBot
+          console.log(
+            chalk.bold.white(chalk.bgMagenta(`✧ CÓDIGO DE VINCULACIÓN ✧`)),
+            chalk.bold.white(chalk.white(codeBot))
+          )
+        }, 3000)
+      }
+    }
+  }
 }
 
 conn.isInit = false;
